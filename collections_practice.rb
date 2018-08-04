1f1c40f8143f4e9dff917772b7f890e1873dd8f9
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-04 01:53:03 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,15 @@
 def sort_array_asc(array)
   array.sort
 end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
