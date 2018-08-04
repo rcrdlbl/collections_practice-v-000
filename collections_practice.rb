ab264bf9f7ee7f0f3db078e6e065f3cb6dae1f4c
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-04 02:26:53 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,5 +47,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index| element="#{element}s" unless index==1 else element = element}
+  array.each_with_index.collect{|element, index| element="#{element}s" unless index==1}
 end
